49519ca2e015154d3aa40663f14586759e0176f8
jae-hyeok <eheh34w@naver.com>
2024-06-04 09:09:31 +0900

    ✨ fect:기본crud

--- src/server.js
+++ src/server.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const {userRouter} = require('./routers/User')
+
+const app = express();
+    
+app.use(express.json())
 
 const server = async() => {
   try{
     const MONGO_URL = "mongodb+srv://eheh34w:KGTE1tJFElnjfsbQ@cluster0.bho2lgx.mongodb.net/BlogService?retryWrites=true&w=majority&appName=Cluster0";
 
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(MONGO_URL,{})
       .then(() => console.log('Connected!'));
     
-    const app = express();
+
     
-    app.use(express.json())
-    
-    app.get('/', (req,res)=>{
+    app.get('/', (req, res)=>{
     
       res.send('<p>some html!!!</p>')
     
     })
-    
+    app.use('/user',userRouter);
     app.listen((3000),()=>{
       console.log("nodemon start");
     })
